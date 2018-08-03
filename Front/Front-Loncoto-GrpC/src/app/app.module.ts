104b83ded2fe4f6ee2cbc39ea73b46bc1b127fc0
Nino <nino@me.com>
2018-08-03 14:55:41 +0200

    Architecture composant 1.0

--- Front/Front-Loncoto-GrpC/src/app/app.module.ts
+++ Front/Front-Loncoto-GrpC/src/app/app.module.ts
@@ -4,10 +4,32 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { ClientComponent } from './components/client/client.component';
+import { OperateurComponent } from './components/operateur/operateur.component';
+import { IntervenantComponent } from './components/intervenant/intervenant.component';
+import { DisplayInterventionsDuclientComponent } from './components/client/subComponents/display-interventions-duclient/display-interventions-duclient.component';
+import { SignalementNvProblemeComponent } from './components/client/subComponents/signalement-nv-probleme/signalement-nv-probleme.component';
+import { DisplayInterventionsComponent } from './components/intervenant/subComponents/display-interventions/display-interventions.component';
+import { DisplayPlanningsComponent } from './components/intervenant/subComponents/display-plannings/display-plannings.component';
+import { EditInterventionsComponent } from './components/intervenant/subComponents/edit-interventions/edit-interventions.component';
+import { EditPlanningsComponent } from './components/operateur/subComponents/edit-plannings/edit-plannings.component';
+import { DisplayReferentielComponent } from './components/operateur/subComponents/display-referentiel/display-referentiel.component';
+import { EditReferentielComponent } from './components/operateur/subComponents/edit-referentiel/edit-referentiel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    OperateurComponent,
+    IntervenantComponent,
+    DisplayInterventionsDuclientComponent,
+    SignalementNvProblemeComponent,
+    DisplayInterventionsComponent,
+    DisplayPlanningsComponent,
+    EditInterventionsComponent,
+    EditPlanningsComponent,
+    DisplayReferentielComponent,
+    EditReferentielComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front/Front-Loncoto-GrpC/src/app/components/operateur/subComponents/display-referentiel/display-referentiel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-referentiel',
+  templateUrl: './display-referentiel.component.html',
+  styleUrls: ['./display-referentiel.component.css']
+})
+export class DisplayReferentielComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
